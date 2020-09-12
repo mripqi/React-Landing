fe4ebbe6e9a78ae6937247c88cf40021e1f0716d
mripqi <mrip95@gmail.com>
2020-09-12 19:11:43 +0700

    commit react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
+import me from './me.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+      <h1>Welcome!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className='nav-flex-container'>
+        <div>Home</div>
+        <div>Contact</div>
+      </div>
+
+      <div className='container'>
+      <img src={me} className="me" alt="me" />
+        <div className='bio'>
+          <ul>
+            <li>
+            <h2>Name     : Muhammad Ripqi</h2> 
+            </li>
+            <li>
+            <h2>Birth    : Ciamis, West Java</h2> 
+            </li>
+            <li>
+            <h2>Location : Medan</h2> 
+            </li>
+            <li>
+            <h2>Job      : Developer</h2> 
+            </li>
+            <li>
+            <h2>Likes    : Cat, Games, Novel</h2>
+            </li>
+          </ul>
+    
+        </div>
+      </div>
     </div>
+
   );
 }
 
